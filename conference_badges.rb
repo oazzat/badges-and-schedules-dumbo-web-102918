394662d579d3e767e0df0e626cd39bd9859ea98e
Omar A <oazzat19@gmail.com>
2018-09-29 17:48:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,8 +3,23 @@ def badge_maker (name)
 end
   
 def batch_badge_creator (array)
+  new_array = []
   array.each do |nam|
-    badge_maker(nam)
+    new_array.push(badge_maker(nam))
   end
+  return new_array
 end
 
+def assign_rooms (array)
+  counter = 1
+  new_array = []
+  array.each do |item|
+    new_array[counter-1] = "Hello, #{item}! You'll be assigned to room #{counter}!"
+    counter +=1 
+  end
+  return new_array
+end 
+
+def printer
+  names = batch_badge_creator
+end
